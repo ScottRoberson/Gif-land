fa8f40483fc3f4291b145ba3f1fcee5247952904
Scott Roberson <srkazan23@gmail.com>
2019-12-06 21:46:26 -0600

    added html, css & JS

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const APIKey = config.API_Key;
+const results = document.querySelector('.results');
+
+//https://api.giphy.com/v1/
+
+//Search End point 
+//https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q${input.value}=&limit=25&offset=0&rating=G&lang=en
+
+//Trending
+//https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=25&rating=G
+//140 x 140
+
+
+const gifs = [];
+
+fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=25&rating=G`)
+  .then(res => res.json())
+    .then(data => gifs.push(...data.data));
+
+    console.log(gifs);
+
+
+    
+ 
+
+
