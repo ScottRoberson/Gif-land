2bf3527f960c026873948fd0860143bfa6bd2151
Scott Roberson <srkazan23@gmail.com>
2019-12-08 21:30:07 -0600

    finished css,started JS

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const APIKey = config.API_Key;
 const results = document.querySelector('.results');
+const trendAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=5&rating=G`
 
-//https://api.giphy.com/v1/
+
+let trendGifs = [];
+//  fetch(trendAPI)
+//   .then(res => res.json())
+//   .then(data => console.log(data.data[0].url));
+
+  async function getTrendingGifs(){
+    const response = await fetch(trendAPI);
+    const json = await response.json();
+    console.log(json.data);
+
+    json.data.forEach(trendGif => {
+      const section = document.querySelector('.trending');
+      const image = document.createElement('img');
+      image.classList.add('trending-gifs');
+      image.src='Mockup/test-gif.webp';
+      section.appendChild(image);
+    })
+  }
+getTrendingGifs();
+
+
+  // console.log(trendGifs);
 
 //Search End point 
 //https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q${input.value}=&limit=25&offset=0&rating=G&lang=en
@@ -19,8 +42,5 @@ const results = document.querySelector('.results');
 
 //     console.log(gifs);
 
-
-    
- 
 
 
