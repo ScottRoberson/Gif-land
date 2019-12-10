cdb28e24b18fc7138f66b720b657cbe89cb5ed27
Scott Roberson <srkazan23@gmail.com>
2019-12-09 21:20:24 -0600

    continued js work

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 const APIKey = config.API_Key;
 const results = document.querySelector('.results');
-const trendAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=5&rating=G`
+const form = document.getElementById('search-form');
 
 
-let trendGifs = [];
-//  fetch(trendAPI)
-//   .then(res => res.json())
-//   .then(data => console.log(data.data[0].url));
+const trendAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=5&rating=G`;
+//const searchAPI = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${searchInput.value}&limit=25&offset=0&rating=G&lang=en`;
 
-  async function getTrendingGifs(){
-    const response = await fetch(trendAPI);
-    const json = await response.json();
-    console.log(json.data);
+const searchHard = `https://api.giphy.com/v1/gifs/search?api_key=KNCtKIAZrRgh6wtrppvYzj6sttmmEZL9&q=three stooges&limit=25&offset=0&rating=G&lang=en`
 
-    json.data.forEach(trendGif => {
-      const section = document.querySelector('.trending');
-      const image = document.createElement('img');
-      image.classList.add('trending-gifs');
-      image.src='Mockup/test-gif.webp';
-      section.appendChild(image);
-    })
-  }
-getTrendingGifs();
+//   async function getTrendingGifs(){
+//     const response = await fetch(trendAPI);
+//     const json = await response.json();
+//     //console.log(json.data[0].images.downsized_medium.url);
 
-
-  // console.log(trendGifs);
-
-//Search End point 
-//https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q${input.value}=&limit=25&offset=0&rating=G&lang=en
-
-//Trending
-//https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=25&rating=G
-//140 x 140
+//     json.data.forEach(trendGif => {
+//       const section = document.querySelector('.trending');
+//       const image = document.createElement('img');
+//       image.classList.add('trending-gifs');
+//       image.src=trendGif.images.downsized_medium.url;
+//       section.appendChild(image);
+      
+//     })
+//   }
+// getTrendingGifs();
 
 
-// const gifs = [];
+// async function getGifs(){
+//  const response = await fetch(searchAPI);
+//  const json = await response.json();
+//  console.log(json);
 
-// fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=25&rating=G`)
-//   .then(res => res.json())
-//     .then(data => gifs.push(...data.data));
 
-//     console.log(gifs);
+// }
+
+form.addEventListener('submit',getSearchGifs);
+
+function getSearchGifs(e){
+const searchInput = document.querySelector('#search-input').value;
+
+fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${searchInput}&limit=25&offset=0&rating=G&lang=en`)
+.then(res => res.json())
+.then(resp_data =>{
+  let gifArray = resp_data.data;
+  showGifs(gifArray);
+});
+
+
+e.preventDefault();
+
+}
+
+function showGifs(gifArray){
+  gifArray.forEach(img =>{
+    console.log(img);
+  })
+}
+
+  
+
+
+
+
+
+
 
 
 
